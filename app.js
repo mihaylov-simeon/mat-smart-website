14ba06be02277ce6434c0685151cdc3543638c1a
Simeon Mihaylov <mihaylov.design@gmail.com>
2024-02-08 20:37:10 +0200

    initial-commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+function scrollBarTracker() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("my__bar").style.width = scrolled + "%";
+  }
+
+  window.addEventListener('scroll', scrollBarTracker);
+
+//
