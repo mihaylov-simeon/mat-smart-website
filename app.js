1bbb2eaacaecd8a9fe1c0a4511096a38822b68e4
Simeon Mihaylov <mihaylov.design@gmail.com>
2024-02-12 17:00:25 +0200

    contact-form-added

--- app.js
+++ app.js
@@ -118,31 +118,3 @@ window.onscroll = function () {
   scrollFunction();
   scrollBarTracker();
 };
-
-// Testimonials
-
-jQuery(document).ready(function($) {
-  "use strict";
-  //  TESTIMONIALS CAROUSEL HOOK
-  $('#customers-testimonials').owlCarousel({
-      loop: true,
-      center: true,
-      items: 3,
-      margin: 0,
-      autoplay: true,
-      dots:true,
-      autoplayTimeout: 7000,
-      smartSpeed: 450,
-      responsive: {
-        0: {
-          items: 1
-        },
-        768: {
-          items: 2
-        },
-        1170: {
-          items: 3
-        }
-      }
-  });
-});
